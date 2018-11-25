c627d731be114222591a9a9fc002189f172a1361
Sebastian Estay <sebastian.estay@icloud.com>
2018-11-25 23:38:34 +0100

    rgb-test-6: pointer

--- rgb-ring-test-6/rgb-ring-test-6.ino
+++ rgb-ring-test-6/rgb-ring-test-6.ino
@@ -4,9 +4,11 @@ const int pwm_b = 11;
 const int nL = 8;
 const int leds[] = {2, 3, 4, 5, 6, 7, 8, 9};
 int decC = 0;
-byte n = B00000001;
 byte a = B00000001;
 int counter = 0;
+int g = 0;
+int b = 255;
+int p = 3;
 
 void setup(){
   Serial.begin(9600);
@@ -16,10 +18,22 @@ void setup(){
 }
 
 void loop(){
-  analogWrite(pwm_g, 255);
-  analogWrite(pwm_b, 0);
+  int i = 0;
+  pt(&i);
+  Serial.print(i);
+  delay(500);
+  //demo2();
+}
 
-  byte a = bitLL();
+void pt(int *a){
+  *a = 5;
+}  
+
+void demo1(){
+  analogWrite(pwm_g, g);
+  analogWrite(pwm_b, b);
+
+  byte a = bitSL();
 
   digitalWrite(2, bitRead(a, 7));
   digitalWrite(3, bitRead(a, 6));
@@ -30,189 +44,60 @@ void loop(){
   digitalWrite(8, bitRead(a, 1));
   digitalWrite(9, bitRead(a, 0));
 
-  delay(500);
+  delay(100);
 
-//    Serial.print(bitRead(a, 7));
-//    Serial.print(bitRead(a, 6));
-//    Serial.print(bitRead(a, 5));
-//    Serial.print(bitRead(a, 4));
-//    Serial.print(bitRead(a, 3));
-//    Serial.print(bitRead(a, 2));
-//    Serial.print(bitRead(a, 1));
-//    Serial.print(bitRead(a, 0));
-//    Serial.print("\n");
-//    delay(500);
-
+  g += 5;
+  b -= 5; 
 }
 
-byte bitLL(){
+byte bitSL(){
   if(counter < 7){
     a = a << 1;
     counter++;
   }
   else{
-   a = B00000001;
-   counter = 0;
+    bitWrite(a, 0, 1);
+    counter = 0; 
   }
   return a;
 }
 
-void bitL(){
-  for(int n = 0; n < 8; ++n){
-    Serial.print(bitRead(a, 7));
-    Serial.print(bitRead(a, 6));
-    Serial.print(bitRead(a, 5));
-    Serial.print(bitRead(a, 4));
-    Serial.print(bitRead(a, 3));
-    Serial.print(bitRead(a, 2));
-    Serial.print(bitRead(a, 1));
-    Serial.print(bitRead(a, 0));
-    Serial.print("\n");
-    delay(500);
-    a = a << 1;
-  }
-  a = B00000001;
-}
+void demo2(){
+  analogWrite(pwm_g, g);
+  analogWrite(pwm_b, b);
 
-void seq_2(){
-  for(int i = 0; i < 8; ++i){
-    //digitalWrite(2, n[0]);
-  }
-  
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-}
-
-void fade_2(){
-  unsigned int gbC[2];
-  int incC = decC == 0 ? 1 : 0;
-  decC ^= decC;
-
-  for(int i = 0; i < 255; ++i){
-    --gbC[decC];
-    ++gbC[incC];
-  
-    setC(gbC[0], gbC[1]);
-    delay(5);
-  }
-}
-
-void seq_1(){
   digitalWrite(2, 1);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-
-  delay(500);
-
-  digitalWrite(2, 0);
   digitalWrite(3, 1);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-
-  delay(500);
-
-  digitalWrite(2, 0);
-  digitalWrite(3, 0);
   digitalWrite(4, 1);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-
-  delay(500);
-
-  digitalWrite(2, 0);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
   digitalWrite(5, 1);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-
-  delay(500);
-
-  digitalWrite(2, 0);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
   digitalWrite(6, 1);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-
-  delay(500);
-  
-  digitalWrite(2, 0);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
   digitalWrite(7, 1);
-  digitalWrite(8, 0);
-  digitalWrite(9, 0);
-
-  delay(500);
-
-  digitalWrite(2, 0);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
   digitalWrite(8, 1);
-  digitalWrite(9, 0);
-
-  delay(500);
-    
-  digitalWrite(2, 0);
-  digitalWrite(3, 0);
-  digitalWrite(4, 0);
-  digitalWrite(5, 0);
-  digitalWrite(6, 0);
-  digitalWrite(7, 0);
-  digitalWrite(8, 0);
   digitalWrite(9, 1);
 
-  delay(500);  
+  delay(100);
+
+  seq();
 }
 
-void fade_1(){
-  unsigned int gbC[2];
-  for(int decC = 0; decC < 2; ++decC){
-    int incC;    
-    if (decC == 1){
-        incC = 0;
-    }
-    else{
-      incC = decC + 1;
-    }
-    for(int i = 0; i < 255; ++i){
-      --gbC[decC];
-      ++gbC[incC];
-      
-      setC(gbC[0], gbC[1]);
-      delay(5);
-    }
+void seq(){
+  while(g < 256 && b > 0){
+    g += 16;
+    b -= 16;
+    Serial.print(g);
+    Serial.print(" ");
+    Serial.print(b);
+    Serial.print("\n");
+    delay(500);  
   }
-}
 
-void setC(unsigned int green, unsigned int blue){
-  analogWrite(pwm_g, green);
-  analogWrite(pwm_b, blue);
+  while(g > 0 && b < 256){
+    g -= 16;
+    b += 16;
+    Serial.print(g);
+    Serial.print(" ");
+    Serial.print(b);
+    Serial.print("\n");
+    delay(500);  
+  }
 }
